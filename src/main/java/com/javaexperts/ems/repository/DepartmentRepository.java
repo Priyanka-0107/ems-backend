49f489f93a0897fd8ffd1634457f45bd3e441417
Priyanka-0107 <priyankakumari3269@gmail.com>
2024-12-04 18:51:40 +0530

    Revert "Initial Commit."
    This reverts commit 8286b29765118759fa1bea97e9ea20ed87112e92.

--- src/main/java/com/javaexperts/ems/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.javaexperts.ems.repository;
-
-import com.javaexperts.ems.entity.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-}
--- src/main/java/com/javaexperts/ems/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.javaexperts.ems.repository;
-
-import com.javaexperts.ems.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
